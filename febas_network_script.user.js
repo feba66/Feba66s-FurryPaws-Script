28a6422c5297cacf47d484e1558467a80886afc1
Feba66 <feba66@users.noreply.github.com>
2022-09-15 02:23:39 +0200

    Update febas_network_script.user.js

--- febas_network_script.user.js
+++ febas_network_script.user.js
@@ -1,13 +1,19 @@
 // ==UserScript==
 // @name         Feba66's Script
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  Collect loads of information
 // @author       feba66 aka fp: Felix#1631601 aka dc: feba66lap#7402
 // @downloadURL  https://github.com/feba66/Feba66s-FurryPaws-Script/raw/main/febas_network_script.user.js
 // @updateURL    https://github.com/feba66/Feba66s-FurryPaws-Script/raw/main/febas_network_script.meta.js
 // @match        https://www.furry-paws.com/kennel/overview
 // @match        https://www.furry-paws.com/dog/index/*
+// @match        https://www.furry-paws.com/dog/refill_water/*
+// @match        https://www.furry-paws.com/dog/refill_food/*
+// @match        https://www.furry-paws.com/dog/play/*
+// @match        https://www.furry-paws.com/dog/clean/*
+// @match        https://www.furry-paws.com/dog/groom/*
+// @match        https://www.furry-paws.com/dog/train/*
 // @icon         https://www.google.com/s2/favicons?domain=furry-paws.com
 // @grant        none
 // ==/UserScript==
